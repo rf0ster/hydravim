905526ee0761288fd3891bdebbbd60c1eb6f1f29
Riley Foster <RFoster@slb.com>
2024-11-05 11:49:41 -0600

    Removed view module.

--- lua/dotnet/nuget/init.lua
+++ lua/dotnet/nuget/init.lua
@@ -1,18 +1,16 @@
 local M = {}
 
 function M.NugetManager()
-	local window = require "view.window"
-	local buffer = require "view.buffer"
 	local nuget = require "nuget.api"
 
 	local buf = vim.api.nvim_create_buf(false, true)
 	local pkgs = nuget.query("Microsoft")
 	for _, pkg in ipairs(pkgs) do
 		local details = string.format("%s %s", pkg.id, pkg.version)
-		buffer.append(buf, {details})
+		---buffer.append(buf, {details})
 	end
 
-	M.window = window.create(0.8, 0.8, buf)
+	--M.window = window.create(0.8, 0.8, buf)
 end
 
 function M.close()
